3b9a7baa27c13ec1c703648208183759ea098ad6
leith <leith@desktop-2j8aqfj-2.home>
2020-07-09 16:14:57 -0500

    WebSocket Communication setup

--- Pirates-of-the-Binks-Game/app.js
+++ Pirates-of-the-Binks-Game/app.js
@@ -9,3 +9,19 @@ app.get('/', function(req, res) {
 app.use('/client', express.static(__dirname + '/client'))
 
 serv.listen(2000)
+console.log("server started")
+
+let io = require('socket.io')(serv, {})
+io.sockets.on('connection', function(socket){
+
+  console.log("socket connection")
+
+  socket.on('binks',function(data){
+    console.log("binks " + data.reason)
+  })
+
+  socket.emit('serverMsg',{
+    msg:"ça va frère le boss tu es en mode lacoste tn et tout?",
+  })
+
+})
