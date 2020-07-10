fc525b531d382dc0a6eb4a87f80f44712b4323bf
leith <leith@desktop-2j8aqfj-2.home>
2020-07-10 13:50:21 -0500

    Added movement system (z,q,s,d)

--- Pirates-of-the-Binks-Game/app.js
+++ Pirates-of-the-Binks-Game/app.js
@@ -16,7 +16,110 @@ app.use('/client', express.static(__dirname + '/client'))
 serv.listen(2000)
 console.log("server started")
 
+//////////////////////////////////////////
+//player and entity class
+//////////////////////////////////////////
+
 let SOCKET_LIST = {}
+
+let Entity = function(){
+
+  let self = {
+
+    x:250,
+    y:250,
+    spdX:0,
+    spdY:0,
+    id:"",
+
+  }
+
+  self.update = function(){
+    self.updatePosition()
+  }
+
+  self.updatePosition = function(){
+    self.x += self.spdX
+    self.y += self.spdY
+  }
+
+  return self
+
+}
+
+let Player = function(id){
+
+  let self = Entity()
+  self.id = id
+  self.number = "" + Math.floor(10 * Math.random())
+  self.pressingRight = false
+  self.pressingLeft = false
+  self.pressingUp = false
+  self.pressingDown = false
+  self.maxSpd = 10
+
+  let super_update = self.update
+  self.update = function(){
+    self.updateSpd()
+    super_update()
+  }
+
+  self.updateSpd = function(){
+    if(self.pressingRight)
+      self.spdX = self.maxSpd
+    else if(self.pressingLeft)
+      self.spdX = -self.maxSpd
+    else
+      self.spdX = 0
+
+    if(self.pressingUp)
+      self.spdY = -self.maxSpd
+    else if(self.pressingDown)
+      self.spdY = self.maxSpd
+    else
+      self.spdY = 0
+  }
+  Player.list[id] = self
+  return self
+
+}
+Player.list = {}
+Player.onConnect = function(socket){
+
+  let player = Player(socket.id)
+
+  socket.on('keyPress', function(data){
+    if (data.inputId === 'left')
+      player.pressingLeft = data.state
+    else if (data.inputId === 'right')
+      player.pressingRight = data.state
+    else if (data.inputId === 'up')
+      player.pressingUp = data.state
+    else if (data.inputId === 'down')
+      player.pressingDown = data.state
+  })
+}
+
+Player.onDisconnect = function(socket){
+  delete Player.list[socket.id]
+}
+
+Player.update = function(){
+  let pack = []
+
+  for(let i in Player.list){
+    let player = Player.list[i]
+
+    player.update()
+
+    pack.push({
+      x:player.x,
+      y:player.y,
+      number:player.number
+    })
+  }
+  return pack;
+}
 
 //////////////////////////////////////////
 //interactions on connection
@@ -27,35 +130,22 @@ let io = require('socket.io')(serv, {})
 io.sockets.on('connection', function(socket){
 
   socket.id = Math.random()
-  socket.x = 0
-  socket.y = 0
-  socket.number = "" + Math.floor(10 * Math.random())
   SOCKET_LIST[socket.id] = socket
+
+  Player.onConnect(socket)
 
   console.log("socket connection. id : " + socket.id)
 
   socket.on('disconnect',function(){
     delete SOCKET_LIST[socket.id]
+    Player.onDisconnect(socket)
   })
 
 })
 
 //updating socket's parameters and sending them to client
 setInterval(function(){
-
-  let pack = []
-
-  for(let i in SOCKET_LIST){
-    let socket = SOCKET_LIST[i]
-    socket.x++
-    socket.y++
-
-    pack.push({
-      x:socket.x,
-      y:socket.y,
-      number:socket.number
-    })
-  }
+  let pack = Player.update();
 
   for(let i in SOCKET_LIST){
     let socket = SOCKET_LIST[i]
