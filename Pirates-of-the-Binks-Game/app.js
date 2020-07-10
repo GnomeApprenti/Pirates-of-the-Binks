c5c0b3d40a57f25c642a57a61d18cf2e631d76d7
leith <leith@desktop-2j8aqfj-2.home>
2020-07-09 19:37:17 -0500

    Multiple WebSocket Connections

--- Pirates-of-the-Binks-Game/app.js
+++ Pirates-of-the-Binks-Game/app.js
@@ -1,3 +1,8 @@
+
+//////////////////////////////////////////
+//initializing server
+//////////////////////////////////////////
+
 let express = require('express')
 let app = express()
 let serv = require('http').Server(app)
@@ -11,17 +16,50 @@ app.use('/client', express.static(__dirname + '/client'))
 serv.listen(2000)
 console.log("server started")
 
+let SOCKET_LIST = {}
+
+//////////////////////////////////////////
+//interactions on connection
+//////////////////////////////////////////
+
+//defining socket's parameters and adding it to the socket list
 let io = require('socket.io')(serv, {})
 io.sockets.on('connection', function(socket){
 
-  console.log("socket connection")
+  socket.id = Math.random()
+  socket.x = 0
+  socket.y = 0
+  socket.number = "" + Math.floor(10 * Math.random())
+  SOCKET_LIST[socket.id] = socket
 
-  socket.on('binks',function(data){
-    console.log("binks " + data.reason)
-  })
+  console.log("socket connection. id : " + socket.id)
 
-  socket.emit('serverMsg',{
-    msg:"ça va frère le boss tu es en mode lacoste tn et tout?",
+  socket.on('disconnect',function(){
+    delete SOCKET_LIST[socket.id]
   })
 
 })
+
+//updating socket's parameters and sending them to client
+setInterval(function(){
+
+  let pack = []
+
+  for(let i in SOCKET_LIST){
+    let socket = SOCKET_LIST[i]
+    socket.x++
+    socket.y++
+
+    pack.push({
+      x:socket.x,
+      y:socket.y,
+      number:socket.number
+    })
+  }
+
+  for(let i in SOCKET_LIST){
+    let socket = SOCKET_LIST[i]
+    socket.emit('newPositions',pack)
+  }
+
+},1000/25)
