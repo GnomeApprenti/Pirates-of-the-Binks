2cb1def9717dacbbcdb91d86175cd498d0088b3d
leith <leith@desktop-2j8aqfj-2.home>
2020-07-10 19:42:40 -0500

    Added random bullets

--- Pirates-of-the-Binks-Game/app.js
+++ Pirates-of-the-Binks-Game/app.js
@@ -121,6 +121,48 @@ Player.update = function(){
   return pack;
 }
 
+let Bullet = function(angle){
+
+  let self = Entity()
+  self.id = Math.random()
+  self.spdX = Math.cos(angle/180*Math.PI)*10
+  self.spdY = Math.sin(angle/180*Math.PI)*10
+
+  self.timer = 0
+  self.toRemove = false
+
+  let super_update = self.update
+  self.update = function(){
+    if(self.timer++ > 100)
+      self.toRemove = true
+    super_update()
+  }
+  Bullet.list[self.id] = self
+  return self
+}
+
+Bullet.list = {}
+
+Bullet.update = function(){
+
+  if(Math.random() < 0.1){
+    Bullet(Math.random()*360)
+  }
+
+  let pack = []
+
+  for(let i in Bullet.list){
+    let bullet = Bullet.list[i]
+
+    bullet.update()
+
+    pack.push({
+      x:bullet.x,
+      y:bullet.y,
+    })
+  }
+  return pack;
+}
 //////////////////////////////////////////
 //interactions on connection
 //////////////////////////////////////////
@@ -145,7 +187,10 @@ io.sockets.on('connection', function(socket){
 
 //updating socket's parameters and sending them to client
 setInterval(function(){
-  let pack = Player.update();
+  let pack = {
+    player:Player.update(),
+    bullet:Bullet.update(),
+  }
 
   for(let i in SOCKET_LIST){
     let socket = SOCKET_LIST[i]
